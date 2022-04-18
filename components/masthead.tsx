af534d3582451d7669551769428cf140b7ec2d6f
Carlos A. Estrada <carlos.estrada@coretechnologies.io>
2022-04-18 19:58:06 -0400

    Show video

---
+++ components/masthead.tsx
@@ -0,0 +1,24 @@
+const Masthead: React.FC = () => {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        className="absolute w-full h-full object-cover"
+      >
+        <source src="/assets/masthead-bg.m4v" type="video/mp4; codecs=hvc1" />
+        <source src="/assets/masthead-bg.webm" type="video/webm; codecs=vp9" />
+      </video>
+      <div className="p-12 font-bold z-10 text-white drop-shadow-[0_5px_3px_rgba(0,0,0,0.4)] text-center flex-1 flex items-center justify-center flex-col">
+        <h1 className="mb-6 text-4xl xl:text-5xl">Margelo</h1>
+        <h2 className="mb-2 text-2xl xl:text-3xl tracking-tight">
+          <span>App Development,</span> <span>done right!</span>
+        </h2>
+      </div>
+    </div>
+  );
+}
+
+export default Masthead;
