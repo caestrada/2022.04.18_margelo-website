f45afce546291a1db9dd5b3e14d0d7a384108755
Carlos A. Estrada <carlos.estrada@coretechnologies.io>
2022-04-18 20:09:40 -0400

    Add logo

--- components/masthead.tsx
+++ components/masthead.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 const Masthead: React.FC = () => {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center">
@@ -11,6 +13,9 @@ const Masthead: React.FC = () => {
         <source src="/assets/masthead-bg.m4v" type="video/mp4; codecs=hvc1" />
         <source src="/assets/masthead-bg.webm" type="video/webm; codecs=vp9" />
       </video>
+      <div className={`flex-grow-0 pt-10 transition-opacity duration-1000`}>
+        <Image src="/assets/logo.svg" width={128/3} height={114/3} alt="logo" />
+      </div>
       <div className="p-12 font-bold z-10 text-white drop-shadow-[0_5px_3px_rgba(0,0,0,0.4)] text-center flex-1 flex items-center justify-center flex-col">
         <h1 className="mb-6 text-4xl xl:text-5xl">Margelo</h1>
         <h2 className="mb-2 text-2xl xl:text-3xl tracking-tight">
