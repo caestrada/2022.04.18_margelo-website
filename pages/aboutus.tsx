23da1907005a4748c8f5bf2d03d4fd6df15581bb
Carlos A. Estrada <carlos.estrada@coretechnologies.io>
2022-04-25 08:17:44 -0400

    Add members

--- pages/aboutus.tsx
+++ pages/aboutus.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Member from "./member";
 
 const AboutUs: React.FC = () => {
   return (
@@ -14,12 +15,66 @@ const AboutUs: React.FC = () => {
         <h2>Our Team</h2>
         <div className="mt-2">the &ldquo;spec-ops&rdquo;</div>
         <div className="mt-10 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 lg:gap-20">
-          <div>member</div>
-          <div>member</div>
-          <div>member</div>
-          <div>member</div>
-          <div>member</div>
-          <div>member</div>
+          <Member
+            id="marc"
+            name="Marc"
+            socialId="@mrousavy"
+            link="https://github.com/caestrada"
+          />
+          <Member
+            id="szymon"
+            name="Szymon"
+            socialId="@szmon20000"
+            link="https://github.com/caestrada"
+          />
+          <Member
+            id="thomas"
+            name="Thomas"
+            socialId="@thomas-coldwell"
+            link="https://github.com/caestrada"
+          />
+          <Member
+            id="christoph"
+            name="Christoph"
+            socialId="@chrispader"
+            link="https://github.com/caestrada"
+          />
+          <Member
+            id="janic"
+            name="Janic"
+            socialId="@janicduplessis"
+            link="https://github.com/caestrada"
+          />
+          <Member
+            id="akinn"
+            name="Akinn"
+            socialId="@akinncar"
+            link="https://github.com/caestrada"
+          />
+          <Member
+            id="mo"
+            name="Mo"
+            socialId="@gorhom"
+            link="https://github.com/caestrada"
+          />
+          <Member
+            id="eric"
+            name="Eric"
+            socialId="@ericvicenti"
+            link="https://github.com/caestrada"
+          />
+          <Member
+            id="matei"
+            name="Matei"
+            socialId="@mateioprea"
+            link="https://github.com/caestrada"
+          />
+          <Member
+            id="viktoria"
+            name="Viktoria"
+            socialId="@viktoria.psd"
+            link="https://github.com/caestrada"
+          />
         </div>
       </div>
     </section>
