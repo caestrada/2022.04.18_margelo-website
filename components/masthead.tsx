3bc8bf2e4006828a02222061d82e745fb2d2f06d
Carlos A. Estrada <carlos.estrada@coretechnologies.io>
2022-05-04 07:52:07 -0400

    Update logo

--- components/masthead.tsx
+++ components/masthead.tsx
@@ -15,15 +15,16 @@ const Masthead: React.FC = () => {
   }
 
   const handleImageLoaded = useCallback(() => {
+    console.log(':::: handle image')
     setImageLoaded(true);
   }, []);
 
   return (
-    <div 
-      ref={refContainer} 
+    <div
+      ref={refContainer}
       className="min-h-screen flex flex-col items-center justify-center sticky top-0 -z-10"
-      style={{transform: `translateY(-${progress * 20}vh)`}}
-      >
+      style={{ transform: `translateY(-${progress * 20}vh)` }}
+    >
       <video
         autoPlay
         loop
@@ -34,8 +35,17 @@ const Masthead: React.FC = () => {
         <source src="/assets/masthead-bg.m4v" type="video/mp4; codecs=hvc1" />
         <source src="/assets/masthead-bg.webm" type="video/webm; codecs=vp9" />
       </video>
-      <div className={`flex-grow-0 pt-10 transition-opacity duration-1000`}>
-        <Image src="/assets/logo.svg" width={128/3} height={114/3} alt="logo" />
+      <div
+        className={`flex-grow-0 pt-10 transition-opacity duration-1000 ${
+          imageLoaded ? "opacity-100" : "opacity-0"
+        }`}
+      >
+        <Image
+          src="/assets/logo.svg"
+          width={128 / 3}
+          height={114 / 3}
+          alt="logo"
+        />
       </div>
       <div className="p-12 font-bold z-10 text-white drop-shadow-[0_5px_3px_rgba(0,0,0,0.4)] text-center flex-1 flex items-center justify-center flex-col">
         <h1 className="mb-6 text-4xl xl:text-5xl">Margelo</h1>
@@ -43,13 +53,18 @@ const Masthead: React.FC = () => {
           <span>App Development,</span> <span>done right!</span>
         </h2>
       </div>
-      <div className={`flex-grow-0 pb-20 md:pb-10 transition-all duration-1000 ${imageLoaded ? 'opacity-100' : 'opacity-0 -translate-y-10'}`}>
-        <Image 
-          src="/assets/arrow-down.png" 
-          width={188 / 3} 
-          height={105/3} 
+      <div
+        className={`flex-grow-0 pb-20 md:pb-10 transition-all duration-1000 ${
+          imageLoaded ? "opacity-100" : "opacity-0 -translate-y-10"
+        }`}
+      >
+        <Image
+          src="/assets/arrow-down.png"
+          width={188 / 3}
+          height={105 / 3}
           alt="scroll down"
-          onLoad={handleImageLoaded} />
+          onLoad={handleImageLoaded}
+        />
       </div>
     </div>
   );
